19260e53abccde99539e7e76ab6106221681fa3d
shawn <s983432@gmail.com>
2017-04-02 11:54:47 +0800

    2017 4/2 11:54

---
+++ C# Practice File/if...else if...else敘述練習/ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("題目:");
+            Console.WriteLine("Visual Studio可以開發哪種應用程式");
+            Console.WriteLine("1.視窗程式 2.Web程式 3.裝置應用程式 4.以上皆是");
+            Console.Write("請作答:");
+            string ans = Console.ReadLine();
+            if (ans == "1"||ans == "2"||ans == "3")
+            {
+                Console.WriteLine("答錯了,正確答案是4");
+            }else if (ans == "4")
+            {
+                Console.WriteLine("答對了");
+            }
+            else
+            {
+                Console.WriteLine("沒有這個選項");
+            }
+            Console.Read();
+
+        }
+    }
+}
