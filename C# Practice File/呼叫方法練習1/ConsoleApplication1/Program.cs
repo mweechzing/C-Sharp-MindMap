5cf660ede14bd91a4256e3e8e73482e92c333294
shawn <s983432@gmail.com>
2017-05-29 16:05:47 +0800

    2017/5/29

---
+++ C# Practice File/呼叫方法練習1/ConsoleApplication1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void login(string x, bool y)
+        {
+            if (y)
+            {
+                Console.WriteLine("{0}先生,歡迎光臨!", x);
+            }
+            else
+            {
+                Console.WriteLine("{0}小姐,歡迎光臨!", x);
+
+            }
+
+            
+
+        }
+        static void Main(string[] args)
+        {
+            string name = "王小華";
+            login(name, true);
+            Console.WriteLine();
+            login("李美美", false);
+            Console.Read();
+        }
+    }
+}
---
+++ C# Practice File/呼叫方法練習2(return)/ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static string login(string x, bool y)
+        {
+            string str = "";
+            if (y)
+            {
+                str = x + "先生,歡迎光臨!";
+            }
+            else
+            {
+                str = x + "小姐,歡迎光臨!";
+            }
+
+            return str;
+
+        }
+        static void Main(string[] args)
+        {
+            string name = "王小華";
+            
+            Console.WriteLine(login(name, true));
+            Console.WriteLine();
+            Console.WriteLine(login("李美美", false));
+            Console.Read();
+        }
+    }
+}
